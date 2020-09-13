f964925eadae8d22c63bd28dc3b352e99b27bf18
TasneemDosoqi <46675287+TasneemDosoqi@users.noreply.github.com>
2020-09-13 10:31:19 +0300

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tasneem.diz/AndroidStudioProjects/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tasneem.diz/Documents/FlutterProjects/Clima"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/tasneem.diz/AndroidStudioProjects/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
